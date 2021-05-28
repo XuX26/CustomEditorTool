e07cdf2e39bb1ae252224f0b01b8f56e1f372d51
Ulysse <ulysse.raillon@hotmail.fr>
2021-05-29 01:10:40 +0200

    [FEAT] New design + nicer code

--- Editor_Tool_Custom/Assets/Scripts/Editor/EditorWindows/AnimatorWindow.cs
+++ Editor_Tool_Custom/Assets/Scripts/Editor/EditorWindows/AnimatorWindow.cs
@@ -7,19 +7,22 @@ using UnityEditor.UIElements;
 using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class AnimatorWindow : EditorWindow
 {
+    public static AnimatorWindow window;
+
     public Animator[] animators { get; private set; }
     private string[] objectsName;
     private int currAnim;
 
     private float _lastEditorTime = 0f;
     private bool _isSimulatingAnimation = false;
+    private float animTime = 0;
+    private float speed = 1;
 
-    public static AnimatorWindow window;
-
-    private PropertyField _propertyField;
+    public List<AnimatorPlayer> currentPlayers = new List<AnimatorPlayer>();
 
     [MenuItem("Window/AnimatorCustom")]
     static void Init()
@@ -36,7 +39,7 @@ public class AnimatorWindow : EditorWindow
 
     void OnGUI()
     {
-        if (window == null) return;
+        if (window == null) Init();
         if (GUILayout.Button("Refresh Animator list"))
             RefreshAnimatorList();
 
@@ -50,9 +53,9 @@ public class AnimatorWindow : EditorWindow
         DisplayAndCenterStrings(new[] {"Nbr animators in scene : " + animators.Length}, false, true);
         EditorGUILayout.Space();
 
-        GUILayout.BeginHorizontal();
+        GUILayout.BeginHorizontal(); 
         GUILayout.Label("Animators : ");
-        currAnim = EditorGUILayout.Popup(currAnim, objectsName);
+        currAnim = EditorGUILayout.Popup(currAnim, objectsName, GUILayout.MaxWidth(200));
         GUILayout.FlexibleSpace();
         if (GUILayout.Button("Select"))
         {
@@ -60,30 +63,62 @@ public class AnimatorWindow : EditorWindow
             SceneView.lastActiveSceneView.FrameSelected();
             EditorGUIUtility.PingObject(Selection.activeObject);
         }
-
         GUILayout.FlexibleSpace();
         GUILayout.EndHorizontal();
 
         GUILayout.Label("Relative controller :  " + animators[currAnim].runtimeAnimatorController.name);
-        //GUILayout.HorizontalSlider(1,0, animators[currAnim].runtimeAnimatorController.animationClips)
-
 
         EditorGUILayout.Space();
-        GUILayout.Label("Clip time " + (_isSimulatingAnimation
-            ? (Time.realtimeSinceStartup - _lastEditorTime).ToString()
-            : "not playing"));
-        //GUILayout.HorizontalSlider()
+        
+        DisplayClipPlayer();
+    }
 
+    void DisplayClipPlayer()
+    {
+        GUILayout.BeginHorizontal();
         if (!_isSimulatingAnimation)
         {
-            if (GUILayout.Button("Play anim"))
+            if (GUILayout.Button("PLAY",GUILayout.MinHeight(38), GUILayout.MinWidth(60)))
                 StartAnimSimulation();
         }
         else
         {
-            if (GUILayout.Button("Stop Anim"))
+            if (GUILayout.Button("STOP",GUILayout.MinHeight(38), GUILayout.MinWidth(60)))
                 StopAnimSimulation();
         }
+        // Var names
+        GUILayout.BeginVertical();
+        GUILayout.Label("Speed");
+        GUILayout.Label("Time");
+        GUILayout.EndVertical();
+        
+        // Float fields
+        float animDuration = animators[currAnim].runtimeAnimatorController.animationClips[0].length;
+        GUILayout.BeginVertical();
+        speed = EditorGUILayout.FloatField(Mathf.Clamp(speed,0,2), GUILayout.MaxWidth(30));
+        animTime = EditorGUILayout.FloatField(Mathf.Clamp(animTime,0,animDuration), GUILayout.MaxWidth(30));
+        GUILayout.EndVertical();
+        
+        // Min Sliders value
+        GUILayout.BeginVertical();
+        GUILayout.Label("0");
+        GUILayout.Label("0");
+        GUILayout.EndVertical();
+        
+        // Sliders
+        GUILayout.BeginVertical();
+        speed = GUILayout.HorizontalSlider(speed, 0, 2f,GUILayout.MinWidth(100), GUILayout.MaxWidth(200));
+        EditorGUILayout.Space(13);
+        animTime = GUILayout.HorizontalSlider(animTime, 0, animDuration, GUILayout.MinWidth(100), GUILayout.MaxWidth(200));
+        GUILayout.EndVertical();
+
+        // Max Sliders value
+        GUILayout.BeginVertical();
+        GUILayout.Label("2");
+        GUILayout.Label(animDuration.ToString());
+        GUILayout.EndVertical();
+
+        GUILayout.EndHorizontal();
     }
 
     void RefreshAnimatorList()
@@ -140,20 +175,20 @@ public class AnimatorWindow : EditorWindow
     }
 
     // --- SimulatingAnimation ---
-
     #region SimulatingAnimation
 
     private void UpdateAnimation()
     {
         if (animators[currAnim] == null) return;
 
-        float animTime = Time.realtimeSinceStartup - _lastEditorTime;
+        animTime += (Time.realtimeSinceStartup - _lastEditorTime - animTime)*speed;
 
         AnimationClip animClip = animators[currAnim].runtimeAnimatorController.animationClips[0];
 
         if (animTime >= animClip.length)
         {
             StopAnimSimulation();
+            animTime = 0;
         }
         else
         {
@@ -187,25 +222,23 @@ public class AnimatorWindow : EditorWindow
             StopAnimSimulation();
         }
     }
-
     #endregion
 }
 
 [Serializable]
 public class AnimatorPlayer
 {
-    public Animator _animator;
-    public AnimationClip[] clips;
+    public Animator Animator{ get; private set; }
+    public AnimationClip[] Clips { get; private set; }
+    public int AnimatorIndex { get; private set; }
+
     public int currClip = 0;
     public float speed = 1;
 
-
-    private int _animatorIndex;
-
     AnimatorPlayer(int animatorIndex)
     {
-        _animatorIndex = animatorIndex;
-        _animator = AnimatorWindow.window.animators[animatorIndex];
-        clips = _animator.runtimeAnimatorController.animationClips;
+        AnimatorIndex = animatorIndex;
+        Animator = AnimatorWindow.window.animators[animatorIndex];
+        Clips = Animator.runtimeAnimatorController.animationClips;
     }
 }
