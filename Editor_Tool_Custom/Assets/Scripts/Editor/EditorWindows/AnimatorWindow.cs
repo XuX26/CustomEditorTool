8596c64f855e1d7edf4fed3a6ee58062a2595c68
Ulysse <ulysse.raillon@hotmail.fr>
2021-05-28 18:05:48 +0200

    [FEAT] Play/Stop button

--- Editor_Tool_Custom/Assets/Scripts/Editor/EditorWindows/AnimatorWindow.cs
+++ Editor_Tool_Custom/Assets/Scripts/Editor/EditorWindows/AnimatorWindow.cs
@@ -11,7 +11,10 @@ public class AnimatorWindow : EditorWindow
 {
     private Animator[] animators;
     private string[] objectsName;
-    private int objectIndex;
+    private int currAnim;
+
+    private float _lastEditorTime = 0f;
+    private bool _isSimulatingAnimation = false;
 
     [MenuItem("Window/CustomAnimator")]
     static void Init()
@@ -19,6 +22,11 @@ public class AnimatorWindow : EditorWindow
         // Get existing open window or if none, make a new one:
         AnimatorWindow window = (AnimatorWindow) EditorWindow.GetWindow(typeof(AnimatorWindow));
         window.Show();
+    }
+
+    private void Update()
+    {
+        Repaint();
     }
 
     void OnGUI()
@@ -33,32 +41,42 @@ public class AnimatorWindow : EditorWindow
             return;
         }
 
+
         DisplayAndCenterStrings(new[] {"Nbr animators in scene : " + animators.Length}, false, true);
         EditorGUILayout.Space();
 
         GUILayout.BeginHorizontal();
         GUILayout.Label("Animators : ");
-        objectIndex = EditorGUILayout.Popup(objectIndex, objectsName);
+        currAnim = EditorGUILayout.Popup(currAnim, objectsName);
         GUILayout.FlexibleSpace();
+
         if (GUILayout.Button("Select"))
         {
-            Selection.activeObject = animators[objectIndex].gameObject;
+            Selection.activeObject = animators[currAnim].gameObject;
             SceneView.lastActiveSceneView.FrameSelected();
             EditorGUIUtility.PingObject(Selection.activeObject);
         }
 
         GUILayout.FlexibleSpace();
         GUILayout.EndHorizontal();
-        GUILayout.Label("Relative controller :  " + animators[objectIndex].runtimeAnimatorController.name);
+        GUILayout.Label("Relative controller :  " + animators[currAnim].runtimeAnimatorController.name);
 
         EditorGUILayout.Space();
+        if(_isSimulatingAnimation)
+            GUILayout.Label("animTime " + (Time.realtimeSinceStartup - _lastEditorTime));
 
-        if (GUILayout.Button("Play anim"))
+        if (!_isSimulatingAnimation)
         {
-            AnimationMode.StartAnimationMode();
-            foreach (Animator animator in animators)
+            if (GUILayout.Button("Play anim"))
             {
-                animator.Play(animator.GetCurrentAnimatorClipInfo(0).ToString());
+                StartAnimSimulation();
+            }
+        }
+        else
+        {
+            if (GUILayout.Button("Stop Anim"))
+            {
+                StopAnimSimulation();
             }
         }
     }
@@ -98,20 +116,70 @@ public class AnimatorWindow : EditorWindow
     {
         if (inARow)
             GUILayout.BeginHorizontal();
-        
+
         foreach (string str in text)
         {
             if (!inARow)
                 GUILayout.BeginHorizontal();
-            
+
             GUILayout.FlexibleSpace();
             GUILayout.Label(str, bold ? EditorStyles.boldLabel : EditorStyles.label);
             GUILayout.FlexibleSpace();
-            
+
             if (!inARow)
                 GUILayout.EndHorizontal();
         }
+
         if (inARow)
             GUILayout.EndHorizontal();
     }
+
+    // --- SimulatingAnimation ---
+    #region SimulatingAnimation
+
+    private void UpdateAnimation()
+    {
+        if (animators[currAnim] == null) return;
+
+        float animTime = Time.realtimeSinceStartup - _lastEditorTime;
+
+        AnimationClip animClip = animators[currAnim].runtimeAnimatorController.animationClips[0];
+
+        if (animTime >= animClip.length)
+        {
+            StopAnimSimulation();
+        }
+        else
+        {
+            if (AnimationMode.InAnimationMode())
+            {
+                AnimationMode.SampleAnimationClip(animators[currAnim].gameObject, animClip, animTime);
+            }
+        }
+    }
+    
+    public void StartAnimSimulation()
+    {
+        AnimationMode.StartAnimationMode();
+        EditorApplication.update -= UpdateAnimation;
+        EditorApplication.update += UpdateAnimation;
+        _lastEditorTime = Time.realtimeSinceStartup;
+        _isSimulatingAnimation = true;
+    }
+
+    public void StopAnimSimulation()
+    {
+        AnimationMode.StopAnimationMode();
+        EditorApplication.update -= UpdateAnimation;
+        _isSimulatingAnimation = false;
+    }
+    
+    private void _OnPlayModeStateChange(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.ExitingEditMode) {
+            StopAnimSimulation();
+        }
+    }
+   
+    #endregion
 }
